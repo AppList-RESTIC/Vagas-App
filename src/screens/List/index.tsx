340429fa44d22b44214b183c96c34731054d345b
Andre Chagas <andreluis_chagas@hotmail.com>
2024-12-01 14:01:23 -0300

    modificações na tela de listagem de vagas(List)

--- src/screens/List/index.tsx
+++ src/screens/List/index.tsx
@@ -1,94 +1,58 @@
-import React from 'react';
-import { Image, FlatList, View, Text } from 'react-native';
-import { Wrapper,Container, ListContainer, TextVagas } from './styles';
+import React, { useState, useEffect } from 'react';
+import { FlatList, Text, View, Image } from 'react-native';
+import axios from 'axios';
+import { Wrapper, Container, ListContainer, TextVagas } from './styles';
 import BGTop from '../../assets/BGTop.png';
 import Logo from '../../components/Logo';
 import VagaCard from '../../components/VagaCard';
-
+import BottomMenu from '../../components/BottomMenu/BottomMenu'; // Importar o componente de menu inferior
 
 export default function List() {
+  const [vagas, setVagas] = useState([]);
 
+  useEffect(() => {
+    const fetchVagas = async () => {
+      try {
+        const response = await axios.get('http://26.161.237.227:3000/vagas'); // Substitua pelo seu IP
+        console.log(response.data); // Verifique se 'status' está presente nos dados retornados
+        setVagas(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar vagas: ", error);
+      }
+    };
 
-    const DATA = [
-        {
-          "id": 1,
-          "titulo": "Desenvolvedor Front-end",
-          "data_cadastro": "2024-06-21",
-          "empresa": "Tech Solutions"
-        },
-        {
-          "id": 2,
-          "titulo": "Analista de Dados",
-          "data_cadastro": "2024-06-18",
-          "empresa": "Data Insights"
-        },
-        {
-          "id": 3,
-          "titulo": "Gerente de Projetos",
-          "data_cadastro": "2024-06-15",
-          "empresa": "Project Masters"
-        },
-        {
-          "id": 4,
-          "titulo": "Gerente de Projetos",
-          "data_cadastro": "2024-06-15",
-          "empresa": "Project Masters"
-        },
-        {
-          "id": 5,
-          "titulo": "Gerente de Projetos",
-          "data_cadastro": "2024-06-15",
-          "empresa": "Project Masters"
-        },
-        {
-          "id": 6,
-          "titulo": "Gerente de Projetos",
-          "data_cadastro": "2024-06-15",
-          "empresa": "Project Masters"
-        },
-        {
-          "id": 7,
-          "titulo": "Gerente de Projetos",
-          "data_cadastro": "2024-06-15",
-          "empresa": "Project Masters"
-        }
-      ]
+    fetchVagas();
+  }, []);
 
-    return (
-        <Wrapper>
-            <Image source={BGTop} style={{maxHeight: 86}}/>
-
-            <Container>
-
-                <Logo />
-                <TextVagas>{DATA.length} vagas encontradas!</TextVagas>
-                <ListContainer>
-                    <FlatList
-                        data={DATA}
-                        keyExtractor={(item) => item.id.toString()}
-                        renderItem={({item}) => 
-                            <VagaCard
-                                id={item.id}
-                                title={item.titulo} 
-                                dataCreated={item.data_cadastro}
-                                company={item.empresa}
-                            />
-                        }
-                        showsVerticalScrollIndicator={true}
-                        ListEmptyComponent={() => (
-                            <View>
-                                <Text>
-                                    Você ainda não tem tarefas cadastradas
-                                </Text>
-                                <Text>
-                                    Crie tarefas e organize seus itens a fazer.
-                                </Text>
-                            </View>
-                        )}
-                    />
-                </ListContainer>
-
-            </Container>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <Image source={BGTop} style={{ maxHeight: 86 }} />
+      <Container>
+        <Logo />
+        <TextVagas>{vagas.length} vagas encontradas!</TextVagas> {/* Verifique o TextVagas */}
+        <ListContainer>
+          <FlatList
+            data={vagas}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <VagaCard
+                id={item.id}
+                title={item.titulo}
+                dataCreated={item.data_cadastro}
+                company={item.empresa}
+                status={item.status} // Adicione o status aqui
+              />
+            )}
+            showsVerticalScrollIndicator={true}
+            ListEmptyComponent={() => (
+              <View>
+                <Text>Não há vagas disponíveis no momento.</Text> {/* Verifique o Text aqui */}
+              </View>
+            )}
+          />
+        </ListContainer>
+      </Container>
+      <BottomMenu /> {/* Adicionar o menu inferior aqui */}
+    </Wrapper>
+  );
 }
